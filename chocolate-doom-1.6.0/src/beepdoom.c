82a23f72077c18c70fee490c8809bc5586875d1d
Kim <kim.lavoie.14@gmail.com>
2013-05-23 15:10:32 -0400

    commented beepdoom.c

--- chocolate-doom-1.6.0/src/beepdoom.c
+++ chocolate-doom-1.6.0/src/beepdoom.c
@@ -13,10 +13,20 @@ void beforeLoop()
 }
 
 void duringLoop()
-{
-	char buffer[10];
-	snprintf(buffer, 10, "%d", (mainPlayer->health));
+{	
+	char buffer[3];
+
+	// convert an int in char[]
+	// snprint(buffer, buffersize, "%d", intToConvert) 
+	snprintf(buffer, 3, "%d", (mainPlayer->health));
+
+	// TMPL_add_var add a variable to the list in the first parameter and return the list.
+	// if the first param is 0, it create a TMPL_varlist and return it.
+	// TMPL_add_var(list, varName1, value1, varName2, value2, ..., 0)
 	variableList = TMPL_add_var(0, "test", buffer, 0);
+
+	// TMPL_write 's purpose is to output the text in a file (in our case, the pipe)
+	// TMPL_write(templateFilename, 0, 0, list, outputFile, errorsOutputFile)
 	TMPL_write("template.xml", 0, 0, variableList, pipeFile, 0);
 }
 
