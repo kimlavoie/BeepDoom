21f3aa47b3154ab4048b4b497ab77e3447b1e11d
Kim <kim.lavoie.14@gmail.com>
2013-06-03 14:08:55 -0400

    update du 3 juin à 14h08

--- chocolate-doom-1.6.0/src/beepdoom.c
+++ chocolate-doom-1.6.0/src/beepdoom.c
@@ -6,6 +6,33 @@ FILE* pipeFile;
 TMPL_varlist *variableList;
 player_t *mainPlayer;
 
+char bufferPlayerHealth[4];
+char bufferArmor[4];
+char bufferPistolAmmos[4];
+char bufferShotgunOwned[2];
+char bufferShotgunAmmos[4];
+char bufferChaingunOwned[2];
+char bufferChaingunAmmos[4];
+char bufferMissileOwned[2];
+char bufferMissileAmmos[4];
+char bufferPlasmaOwned[2];
+char bufferPlasmaAmmos[4];
+char bufferBfgOwned[2];
+char bufferBfgAmmos[4];
+char bufferChainsawOwned[2];
+char bufferSuperShotgunOwned[2];
+char bufferSuperShotgunAmmos[4];
+char bufferActualWeapon[2];
+char bufferAttacking[2];
+char bufferPlayerX[10];
+char bufferPlayerY[10];
+char bufferPlayerZ[10];
+char bufferAttackerHealth[4];
+char bufferAttackerX[10];
+char bufferAttackerY[10];
+char bufferAttackerZ[10];
+char bufferAction[2];
+
 void beforeLoop()
 {
 	pipeFile = fopen("pipe.pipe", "w");
@@ -14,34 +41,6 @@ void beforeLoop()
 
 void duringLoop()
 {	
-	char bufferPlayerHealth[4];
-	char bufferArmor[4];
-	char bufferPistolAmmos[4];
-	char bufferShotgunOwned[2];
-	char bufferShotgunAmmos[4];
-	char bufferChaingunOwned[2];
-	char bufferChaingunAmmos[4];
-	char bufferMissileOwned[2];
-	char bufferMissileAmmos[4];
-	char bufferPlasmaOwned[2];
-	char bufferPlasmaAmmos[4];
-	char bufferBfgOwned[2];
-	char bufferBfgAmmos[4];
-	char bufferChainsawOwned[2];
-	char bufferSuperShotgunOwned[2];
-	char bufferSuperShotgunAmmos[4];
-	char bufferActualWeapon[2];
-	char bufferAttacking[2];
-	char bufferPlayerX[10];
-	char bufferPlayerY[10];
-	char bufferPlayerZ[10];
-	char bufferAttackerHealth[4];
-	char bufferAttackerX[10];
-	char bufferAttackerY[10];
-	char bufferAttackerZ[10];
-	char bufferAction[2];
-
-
 	// convert an int in char[]
 	// snprint(buffer, buffersize, "%d", intToConvert) 
 	snprintf(bufferPlayerHealth, 4, "%d", (mainPlayer->health));
@@ -54,14 +53,17 @@ void duringLoop()
 	snprintf(bufferBfgAmmos, 4, "%d", (mainPlayer->ammo[wp_bfg]));
 	snprintf(bufferSuperShotgunAmmos, 4, "%d", (mainPlayer->ammo[wp_supershotgun]));
 	snprintf(bufferActualWeapon, 2, "%d", (mainPlayer->readyweapon));
-/*
-	snprintf(bufferPlayerX, 10, "%f", (mainPlayer->mo->x));
-	snprintf(bufferPlayerY, 10, "%d", (mainPlayer->mo->y));
-	snprintf(bufferPlayerZ, 10, "%d", (mainPlayer->mo->z));
-	snprintf(bufferAttackerX, 10, "%d", (mainPlayer->attacker->x));
-	snprintf(bufferAttackerY, 10, "%d", (mainPlayer->attacker->y));
-	snprintf(bufferAttackerZ, 10, "%d", (mainPlayer->attacker->z));
-*/
+
+	if(mainPlayer->mo != 0){
+		snprintf(bufferPlayerX, 10, "%f", (mainPlayer->mo->momx));
+		snprintf(bufferPlayerY, 10, "%f", (mainPlayer->mo->momy));
+		snprintf(bufferPlayerZ, 10, "%f", (mainPlayer->mo->momz));
+	}
+	if(mainPlayer->attacker != 0){
+		snprintf(bufferAttackerX, 10, "%f", (mainPlayer->attacker->x));
+		snprintf(bufferAttackerY, 10, "%f", (mainPlayer->attacker->y));
+		snprintf(bufferAttackerZ, 10, "%f", (mainPlayer->attacker->z));
+	}
 
 	bufferShotgunOwned[0] = (mainPlayer->weaponowned[wp_shotgun] ? 'T' : 'F');
 	bufferChaingunOwned[0] = (mainPlayer->weaponowned[wp_chaingun] ? 'T' : 'F');
@@ -104,14 +106,12 @@ void duringLoop()
 	variableList = TMPL_add_var(variableList, "superShotgunAmmos", bufferSuperShotgunAmmos, 0);
 	variableList = TMPL_add_var(variableList, "actualWeapon", bufferActualWeapon, 0);
 	variableList = TMPL_add_var(variableList, "attacking", bufferAttacking, 0);
-/*
 	variableList = TMPL_add_var(variableList, "playerX", bufferPlayerX, 0);
 	variableList = TMPL_add_var(variableList, "playerY", bufferPlayerY, 0);
 	variableList = TMPL_add_var(variableList, "playerZ", bufferPlayerZ, 0);
 	variableList = TMPL_add_var(variableList, "attackerX", bufferAttackerX, 0);
 	variableList = TMPL_add_var(variableList, "attackerY", bufferAttackerY, 0);
 	variableList = TMPL_add_var(variableList, "attackerZ", bufferAttackerZ, 0);
-*/
 	variableList = TMPL_add_var(variableList, "attackerHealth", bufferAttackerHealth, 0);
 	variableList = TMPL_add_var(variableList, "action", bufferAction, 0);
 
